0ff9f16439e51517fd7d8f9ed87316b30d6bd3e6
luis <luiscastrodev@gmail.com>
2020-11-03 21:50:36 -0300

    Variables
    Variables

---
+++ Variables/src/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    val name = "Name"
+    val name2 :String = "Name"
+
+    //name = "2" //imutable
+
+    var name3 = "Name"
+    name3 = "New Name"
+
+    var age = 1
+    var age2: Int = 20
+
+}

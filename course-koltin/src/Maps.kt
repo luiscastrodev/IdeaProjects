089a663817b7961737a4c04636115778c12f99be
luis <luiscastrodev@gmail.com>
2020-11-05 21:56:46 -0300

    map
    map

---
+++ course-koltin/src/Maps.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(){
+    val binaryRepresentation = TreeMap<Char,String>()
+
+    for (c in 'A'..'F'){
+
+        var binary = Integer.toBinaryString(c.toInt())
+
+        binaryRepresentation[c] = binary
+    }
+
+    for ((letter, binary) in  binaryRepresentation) {
+        println("$letter - $binary")
+    }
+}

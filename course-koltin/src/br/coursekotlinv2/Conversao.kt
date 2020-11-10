4d66f759560731d10b30ef8cca98ece3fcebb72a
luis <luiscastrodev@gmail.com>
2020-11-09 21:18:25 -0300

    Conversoes
    Conversoes

---
+++ course-koltin/src/br/coursekotlinv2/Conversao.kt
@@ -0,0 +1,20 @@
+package br.coursekotlinv2
+
+import kotlin.experimental.xor
+
+
+fun main(){
+
+    var num1 : Int = 10
+
+    num1.toString()
+
+    var num2 = 10
+
+    num2.toLong()
+
+    var n3 : Short? = null
+
+    println(n3?.toInt())
+
+}
